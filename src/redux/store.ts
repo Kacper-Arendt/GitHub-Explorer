b08ba686f691ee531e9ad3efe4eeedb52d89dc1b
ArendtKacper <k.arendt@gridaly.com>
2023-10-14 00:04:00 +0200

    feat add redux-saga

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from '@redux-saga/core';
 
 // REDUCERS
 import { reducer } from 'src/redux/slices/reducer';
 
+// SAGA
+import rootSaga from 'src/redux/sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
 export const store = configureStore({
 	reducer,
+	middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
