8fd2dcac720885bf551302e8a3928adf9b6a49e0
ArendtKacper <k.arendt@gridaly.com>
2023-10-15 11:33:15 +0200

    feat add not found page

--- src/components/errors/NotFound.tsx
+++ src/components/errors/NotFound.tsx
@@ -1,13 +1,16 @@
-// REDUX
-
-// HOOKS
-
-// MODELS
-
-// COMPONENTS
+import { Link } from 'react-router-dom';
 
 // STYLES
-
+import 'src/components/errors/styles.scss';
 // UTILS
+import { routes } from 'src/routes';
 
-export const NotFound = () => <>Not Found</>;
+export const NotFound = () => (
+	<div className="not-found">
+		<h1 className="not-found__title">Page Not Found</h1>
+		<p className="not-found__description">Sorry, the page you are looking for does not exist.</p>
+		<Link to={routes.userProjects.path} className="not-found__go-back">
+			Go back to the home page
+		</Link>
+	</div>
+);
